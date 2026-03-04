567655e3851a2bb0853bef83f72b51277011eb98
Shreshtha97 <100791584+Shreshtha97@users.noreply.github.com>
2026-03-04 22:40:34 +0530

    Update TodoForm.js

--- Interactive-ToDo-App/src/components/TodoForm.js
+++ Interactive-ToDo-App/src/components/TodoForm.js
@@ -1,1 +1,26 @@
+import React, { useState } from "react";
 
+function TodoForm({ addTodo }) {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text) return;
+    addTodo(text);
+    setText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default TodoForm;

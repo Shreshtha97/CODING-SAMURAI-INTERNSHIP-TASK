c0351e65fa68e67786072b3c99c6b7d412d000a5
Shreshtha97 <100791584+Shreshtha97@users.noreply.github.com>
2026-03-04 22:41:03 +0530

    Update TodoItem.js

--- Interactive-ToDo-App/src/components/TodoItem.js
+++ Interactive-ToDo-App/src/components/TodoItem.js
@@ -1,1 +1,21 @@
+import React from "react";
 
+function TodoItem({ todo, toggleComplete, deleteTodo }) {
+  return (
+    <div>
+      <span
+        onClick={() => toggleComplete(todo.id)}
+        style={{
+          textDecoration: todo.completed ? "line-through" : "none",
+          cursor: "pointer"
+        }}
+      >
+        {todo.text}
+      </span>
+
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default TodoItem;

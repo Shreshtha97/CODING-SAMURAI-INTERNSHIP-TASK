a846b6b78ad79c94816099a4a78d3944c584c566
Shreshtha97 <100791584+Shreshtha97@users.noreply.github.com>
2026-03-04 22:43:53 +0530

    Update App.js

--- Interactive-ToDo-App/src/App.js
+++ Interactive-ToDo-App/src/App.js
@@ -1,1 +1,47 @@
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Interactive To-Do List</h1>
+
+      <TodoForm addTodo={addTodo} />
+
+      <TodoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        deleteTodo={deleteTodo}
+      />
+    </div>
+  );
+}
+
+export default App;
